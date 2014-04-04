449e3d5b3306f5f60350f69ddbe80e4e97e26120
damon <398846606@qq.com>
2014-04-04 22:43:58 +0800

    Init my first node project.

---
+++ routes/index.js
@@ -0,0 +1,55 @@
+
+/*
+ * GET home page.
+ */
+
+// res.render 调用模板引擎，并将其产生的页面直接返回给ke'hu'duan
+// exports.index = function(req, res){
+//   res.render('index', { title: 'Express' });
+// };
+
+// exports.hello = function(req, res){
+// 	res.send('The time is ' + new Date().toString());
+// };
+
+
+/*
+ * GET home page.
+ */
+
+// res.render 调用模板引擎，并将其产生的页面直接返回给客户端
+exports.index = function(req, res){
+	res.render('index', {title: 'Express'});
+}
+
+exports.hello = function(req, res){
+	res.send('1');
+}
+
+exports.user = function(req, res){
+
+}
+
+exports.post = function(req, res){
+
+}
+
+exports.reg = function(req, res){
+
+}
+
+exports.doReg = function(req, res){
+
+}
+
+exports.login = function(req, res){
+
+}
+
+exports.doLogin = function(req, res){
+
+}
+
+exports.logout = function(req, res){
+
+}
