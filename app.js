3c3e78c2ad3574d99cf3cfca769642de558696a1
damon <398846606@qq.com>
2014-04-14 13:10:51 +0800

    Ouput ENV when http server start.

--- app.js
+++ app.js
@@ -81,5 +81,5 @@ if ('development' == app.get('env')) {
 
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port') + ', env: ' + app.get('env'));
 });
